467ddecd2c9234b18e3464545e81baf4473dfb67
Ubuntu <ubuntu@ip-10-99-214-199.ec2.internal>
2016-09-09 13:44:59 +0000

    mesos master and disable scala style

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -7,15 +7,17 @@
 #export SPARK_WORKER_INSTANCES=1
 #export SPARK_WORKER_CORES=2
 
-export HADOOP_HOME="/root/persistent-hdfs"
-export SPARK_MASTER_IP=ec2-54-237-113-55.compute-1.amazonaws.com
+#export HADOOP_HOME="/root/persistent-hdfs"
+#export SPARK_MASTER_IP=ec2-54-237-113-55.compute-1.amazonaws.com
 #export MASTER=`cat /root/spark-ec2/cluster-url`
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so
 
-export SPARK_SUBMIT_LIBRARY_PATH="$SPARK_SUBMIT_LIBRARY_PATH:/root/persistent-hdfs/lib/native/"
-export SPARK_SUBMIT_CLASSPATH="$SPARK_CLASSPATH:$SPARK_SUBMIT_CLASSPATH:/root/persistent-hdfs/conf"
+#export SPARK_SUBMIT_LIBRARY_PATH="$SPARK_SUBMIT_LIBRARY_PATH:/root/persistent-hdfs/lib/native/"
+#export SPARK_SUBMIT_CLASSPATH="$SPARK_CLASSPATH:$SPARK_SUBMIT_CLASSPATH:/root/persistent-hdfs/conf"
+
+export SPARK_EXECUTOR_URI=
 
 # Bind Spark's web UIs to this machine's public EC2 hostname:
 export SPARK_PUBLIC_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
